c6f9b09908d6bd75a96f8fd1414a7b641b92f676
Natan Silva <104787222+INatanSilva@users.noreply.github.com>
2024-12-03 00:33:26 +0000

    CORES MODIFICADAS
    Verde minimalista

--- app/src/main/java/com/example/mobile_teste/LoginTela.kt
+++ app/src/main/java/com/example/mobile_teste/LoginTela.kt
@@ -2,6 +2,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -11,7 +12,6 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -44,7 +44,8 @@ fun LoginScreen() {
                 modifier = Modifier
                     .padding(16.dp)
                     .fillMaxWidth()
-                    .background(Color(0xFFFFFFFF), shape = RoundedCornerShape(16.dp)) // Fundo dos campos
+                    .background(Color(0xFFFFFFFF), shape = RoundedCornerShape(16.dp)) // Fundo branco da coluna
+                    .border(2.dp, Color(0xFFB8D8B7), RoundedCornerShape(20.dp)) // Borda verde mais suave
                     .padding(24.dp)
             ) {
                 Text(
